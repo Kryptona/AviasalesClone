e6c4625a48018cada9e8fb45650d003a1fac651f
Дарья <dasha.seredenko.1997@gmail.com>
2021-11-19 00:55:27 +0400

    Верстка карточки рейсов на мокковых данных

--- src/components/Card/Card.tsx
+++ src/components/Card/Card.tsx
@@ -1,10 +1,32 @@
 import React from 'react';
 import styles from './Card.scss';
+import logo from '../../img/S7 Logo.png';
 
 const Card = () => {
-  return <div className={styles.root}>
 
-  </div>;
+
+  return (
+    <div className={styles.root}>
+      <div className={styles.title}>
+        <div className={styles.price}>13 400 P</div>
+        <img className={styles.logo} src={logo} />
+      </div>
+      <div className={styles.content}>
+        {renderSection('mow - hkt', '10:45 - 08:00')}
+        {renderSection('в пути', '21ч 15м')}
+        {renderSection('2 пересадки', 'HKG, JNB')}
+      </div>
+    </div>
+  );
 };
 
 export default Card;
+
+const renderSection = (title: string, info: string) => {
+  return (
+      <div className={styles.renderSection}>
+        <div className={styles.title}>{title}</div>
+        <div className={styles.info}>{info}</div>
+      </div>
+  );
+};
