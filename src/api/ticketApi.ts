d9db0d0a993dc90dc3f074bec6e7b6a17b27babc
Дарья <dasha.seredenko.1997@gmail.com>
2021-11-30 15:11:22 +0400

    axios retry первая версия

--- src/api/ticketApi.ts
+++ src/api/ticketApi.ts
@@ -1,15 +1,17 @@
 import {Ticket} from '../domain/Ticket';
 import axios, {AxiosResponse} from 'axios';
 import {SearchResponse} from '../domain/SearchResponse';
-import {TicketsResponse} from "../domain/TicketsResponse";
+import {TicketsResponse} from '../domain/TicketsResponse';
+import axiosRetry from "axios-retry";
 
 function getTickets(): Promise<Ticket[]> {
-  return axios
-    .get('https://front-test.beta.aviasales.ru/search')
-    .then((id: AxiosResponse<SearchResponse>) => id.data.searchId)
-    .then((id) => axios.get(`https://front-test.beta.aviasales.ru/tickets?searchId=${id}`))
-    .then((tickets: AxiosResponse<TicketsResponse>) => tickets.data.tickets);
-    // .then((tickets: AxiosResponse<TicketsResponse>) => tickets.data.tickets.slice(0, 20));
+  return (
+    axios
+      .get('https://front-test.beta.aviasales.ru/search', {'axios-retry': {retries: 1}})
+      .then((id: AxiosResponse<SearchResponse>) => id.data.searchId)
+      .then((id) => axios.get(`https://front-test.beta.aviasales.ru/tickets?searchId=${id}`))
+      .then((tickets: AxiosResponse<TicketsResponse>) => tickets.data.tickets)
+  );
 }
 
 export const ticketApi = {
